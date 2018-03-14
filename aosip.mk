afdc586bbd6ba85e8c2dfa9dfb784d76ed1334d2
DarkLord1731 <darklord1731@gmail.com>
2018-03-14 11:47:45 +0530

    on7xelet: AOSip Brignup

--- aosip.mk
+++ aosip.mk
@@ -20,11 +20,11 @@ $(call inherit-product, device/samsung/on7xelte/device.mk)
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSip stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_on7xelte
+PRODUCT_NAME := aosip_on7xelte
 PRODUCT_DEVICE := on7xelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
